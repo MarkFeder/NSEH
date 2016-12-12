de5fe9271c0c01a1e8d8b1399a4893339539454d
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2016-12-12 12:58:15 +0100

    Game Over

---
+++ Assets/Scripts/General Scripts/TestGameOver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NSEH
+{
+    public class TestGameOver : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.O))
+            {
+                GetComponent<GameManager_Master>().CallEventGameOver();
+            }
+        }
+    }
+}
+
