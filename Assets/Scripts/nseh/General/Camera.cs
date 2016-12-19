4dda3701554f3dc7eac9801e7f3c94497e3374fb
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2016-12-19 10:01:54 +0100

    Camera final script

--- Assets/Scripts/nseh/General/Camera.cs
+++ Assets/Scripts/nseh/General/Camera.cs
@@ -13,6 +13,14 @@ namespace nseh.General
         public Vector3 Midpoint;
         public Vector3 Position;
         private Vector3 velocity = Vector3.zero;
+        [SerializeField]
+        private float xMin;
+        [SerializeField]
+        private float xMax;
+        [SerializeField]
+        private float yMin;
+        [SerializeField]
+        private float yMax;
 
         // Use this for initialization
         void Start()
@@ -28,15 +36,21 @@ namespace nseh.General
             distance = Player1.position - Player2.position;
             Midpoint =(Player2.position + Player1.position )/2;
             //MOVE CAMERA
-            if (distance.x > -2 )
+            if (Mathf.Abs(distance.x) < 2 && Mathf.Abs(distance.y) < 2)
             {
-               
-                Position =  new Vector3 (Midpoint.x, 1.5f, -10);
+
+                Position = new Vector3(Mathf.Clamp(Midpoint.x, xMin, xMax), Mathf.Clamp(Midpoint.y, yMin, yMax), -10);
+                transform.position = Vector3.SmoothDamp(transform.position, Position, ref velocity, 0.15f);
+            }
+            else if (Mathf.Abs(distance.x) < 4 && Mathf.Abs(distance.y) < 4 )
+            {
+
+                Position = new Vector3(Mathf.Clamp(Midpoint.x, xMin, xMax), Mathf.Clamp(Midpoint.y, yMin, yMax), -15);
                 transform.position = Vector3.SmoothDamp(transform.position, Position, ref velocity, 0.15f);
             }
             else
             {
-                Position = new Vector3(Midpoint.x, 1.5f, -20);
+                Position = new Vector3(Mathf.Clamp(Midpoint.x,xMin,xMax), Mathf.Clamp(Midpoint.y,yMin,yMax), -20);
                 transform.position = Vector3.SmoothDamp(transform.position, Position, ref velocity, 0.15f);
             }
            
