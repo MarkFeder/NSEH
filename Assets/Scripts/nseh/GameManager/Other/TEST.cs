73476d332df8ee61bf01caa97b5fddc619bca1d0
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-02-12 18:58:58 +0100

    Escena test sin pasar por el menú

---
+++ Assets/Scripts/nseh/GameManager/Other/TEST.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nseh.GameManager;
+
+public class TEST : MonoBehaviour {
+
+    MenuManager _MenuManager;
+    GameObject _prospector;
+    // Use this for initialization
+    void Start ()
+    {
+        _MenuManager = GameManager.Instance.Find<MenuManager>();
+        _prospector = Resources.Load("SirProspector") as GameObject;
+        Debug.Log(_MenuManager);
+        _MenuManager.ChangePlayers(1);
+        _MenuManager.Adding(_prospector);
+        _MenuManager.ChangeStates();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
