561c89daf2746106ce20eff8317914a6920c48ba
javivecino <javier.vecino.martinez@alumnos.upm.es>
2017-02-28 15:51:43 +0100

    Item spawn scripts updated, also fixes in cloud material and player two input

--- Assets/Scripts/nseh/Gameplay/Entities/Environment/SpawnItemPoint.cs
+++ Assets/Scripts/nseh/Gameplay/Entities/Environment/SpawnItemPoint.cs
@@ -1,66 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using nseh.GameManager;
+using nseh.Gameplay.Gameflow;
 using UnityEngine;
 
-public class SpawnItemPoint : MonoBehaviour {
+namespace nseh.Gameplay.Entities.Environment
+{
+    public class SpawnItemPoint : MonoBehaviour
+    {
 
-    // Use this for initialization
+        // Use this for initialization
 
-    [SerializeField]
-    private List<GameObject> StandardItems;
-    [SerializeField]
-    private List<GameObject> SpecialItems;
-    [SerializeField]
-    private List<GameObject> SpecialBuffs;
-    private GameObject instancedItem;
-    private bool instanced;
-    private ItemSpawn_Event _itemSpawnEvent;
-    
+        [SerializeField]
+        private List<GameObject> StandardItems;
+        [SerializeField]
+        private List<GameObject> SpecialItems;
+        [SerializeField]
+        private List<GameObject> SpecialBuffs;
+        private GameObject instancedItem;
+        private bool instanced;
+        private ItemSpawn_Event _itemSpawnEvent;
 
-	void Start () {
-        GameObject _spawnItemPoint = this.gameObject;
-        _itemSpawnEvent = GameManager.Instance.Find<LevelManager>().Find<ItemSpawn_Event>();
-        _itemSpawnEvent.RegisterSpawnItemPoint(_spawnItemPoint);
-    }
-	
-	// Update is called once per frame
-	void Update () {
 
-		if(instanced = true && instancedItem == null)
+        void Start()
         {
             instanced = false;
+            GameObject _spawnItemPoint = this.gameObject;
+            _itemSpawnEvent = GameManager.GameManager.Instance.Find<LevelManager>().Find<ItemSpawn_Event>();
+            _itemSpawnEvent.RegisterSpawnItemPoint(_spawnItemPoint);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (instanced = true && instancedItem == null)
+            {
+                instanced = false;
+                _itemSpawnEvent.toggleSpawn();
+            }
+
+        }
+
+        public void Spawn()
+        {
+            float dice = Random.value;
+
+            if (dice <= 0.8)
+            {
+                //Standard buffs
+                int randomStandardItem = (int)Random.Range(0, StandardItems.Count);
+                instancedItem = Instantiate(StandardItems[randomStandardItem], this.transform.position, this.transform.rotation);
+            }
+
+            if (0.8f < dice && dice <= 0.9f)
+            {
+                //Special buffs
+                int randomSpecialItem = (int)Random.Range(0, StandardItems.Count);
+                instancedItem = Instantiate(StandardItems[randomSpecialItem], this.transform.position, this.transform.rotation);
+            }
+
+            if (0.9f < dice && dice <= 1)
+            {
+                //Debuffs
+                int randomDebuffItem = (int)Random.Range(0, StandardItems.Count);
+                instancedItem = Instantiate(StandardItems[randomDebuffItem], this.transform.position, this.transform.rotation);
+            }
+
             _itemSpawnEvent.toggleSpawn();
+            instanced = true;
         }
-
-	}
-
-    public void Spawn()
-    {
-        float dice = Random.value;
-
-        if (dice <= 0.8)
-        {
-            //Standard buffs
-            int randomStandardItem = (int)Random.Range(0, StandardItems.Count);
-            instancedItem = Instantiate(StandardItems[randomStandardItem], this.transform.position, this.transform.rotation);
-        }
-
-        if (0.8f < dice && dice <= 0.9f)
-        {
-            //Special buffs
-            int randomSpecialItem = (int)Random.Range(0, StandardItems.Count);
-            instancedItem = Instantiate(StandardItems[randomSpecialItem], this.transform.position, this.transform.rotation);
-        }
-
-        if (0.9f < dice && dice <= 1)
-        {
-            //Debuffs
-            int randomDebuffItem = (int)Random.Range(0, StandardItems.Count);
-            instancedItem = Instantiate(StandardItems[randomDebuffItem], this.transform.position, this.transform.rotation);
-        }
-
-        _itemSpawnEvent.toggleSpawn();
-        instanced = true;
     }
 }
--- Assets/Scripts/nseh/Gameplay/Gameflow/ItemSpawn_Event.cs
+++ Assets/Scripts/nseh/Gameplay/Gameflow/ItemSpawn_Event.cs
@@ -6,56 +6,59 @@ using nseh.Gameplay.Entities.Environment;
 using nseh.Utils;
 using UnityEngine;
 
-public class ItemSpawn_Event : LevelEvent {
-
-    List<GameObject> _spawnItemPoints;
-    float eventStart = Constants.Events.Tar_Event.EVENT_START;
-    private float elapsedTime;
-    public bool canSpawn;
-
-    override public void Setup(LevelManager lvlManager)
+namespace nseh.Gameplay.Gameflow
+{
+    public class ItemSpawn_Event : LevelEvent
     {
-        base.Setup(lvlManager);
-        _spawnItemPoints = new List<GameObject>();
-    }
+        List<GameObject> _spawnItemPoints;
+        float eventStart = Constants.Events.Tar_Event.EVENT_START;
+        private float elapsedTime;
+        public bool canSpawn;
 
-    public override void ActivateEvent()
-    {
-        IsActivated = true;
-        canSpawn = true;
-    }
-
-    public override void EventRelease()
-    {
-        IsActivated = false;
-    }
-
-    public override void EventTick()
-    {
-        if (canSpawn)
+        override public void Setup(LevelManager lvlManager)
         {
-            ChooseSpawnPoint();
+            base.Setup(lvlManager);
+            _spawnItemPoints = new List<GameObject>();
         }
-    }
 
-    public void RegisterSpawnItemPoint(GameObject lightToRegister)
-    {
-        _spawnItemPoints.Add(lightToRegister);
-    }
-
-    public void toggleSpawn()
-    {
-        canSpawn = !canSpawn;
-    }
-
-    void ChooseSpawnPoint()
-    {
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime >= Constants.Events.Tar_Event.EVENT_START)
+        public override void ActivateEvent()
         {
-            int randomSpawn = (int)Random.Range(0, _spawnItemPoints.Count);
-            _spawnItemPoints[randomSpawn].GetComponent<SpawnItemPoint>().Spawn();
-            elapsedTime = 0;
+            IsActivated = true;
+            canSpawn = true;
+        }
+
+        public override void EventRelease()
+        {
+            IsActivated = false;
+        }
+
+        public override void EventTick()
+        {
+            if (canSpawn)
+            {
+                ChooseSpawnPoint();
+            }
+        }
+
+        public void RegisterSpawnItemPoint(GameObject lightToRegister)
+        {
+            _spawnItemPoints.Add(lightToRegister);
+        }
+
+        public void toggleSpawn()
+        {
+            canSpawn = !canSpawn;
+        }
+
+        void ChooseSpawnPoint()
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= Constants.Events.Tar_Event.EVENT_START)
+            {
+                int randomSpawn = (int)Random.Range(0, _spawnItemPoints.Count);
+                _spawnItemPoints[randomSpawn].GetComponent<SpawnItemPoint>().Spawn();
+                elapsedTime = 0;
+            }
         }
     }
 }
