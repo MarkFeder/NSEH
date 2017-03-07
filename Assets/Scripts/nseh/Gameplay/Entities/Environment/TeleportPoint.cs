f525da2b487aa48943dbc9d0bf23e94f512186e8
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-03-07 20:20:46 +0100

    First steps in teleport logic!

---
+++ Assets/Scripts/nseh/Gameplay/Entities/Environment/TeleportPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPoint : MonoBehaviour {
+
+    [SerializeField]
+    private List<GameObject> TeleportPoints;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetAxis("Vertical")>0)
+        {
+            Debug.Log(Input.GetAxis("Vertical"));
+            int randomStandardItem = (int)Random.Range(0, TeleportPoints.Count);
+            other.transform.position = TeleportPoints[randomStandardItem].transform.position;
+        }
+    }
+}
