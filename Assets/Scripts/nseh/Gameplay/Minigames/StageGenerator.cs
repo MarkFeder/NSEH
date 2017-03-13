a2e2352161140fbb7aecf3c8bf74b9951fd11c76
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-03-13 01:57:43 +0100

    Changes in Minigame Scene

--- Assets/Scripts/nseh/Gameplay/Minigames/StageGenerator.cs
+++ Assets/Scripts/nseh/Gameplay/Minigames/StageGenerator.cs
@@ -2,53 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace nseh.Gameplay.Minigames
+public class StageGenerator : MonoBehaviour
 {
-    public class StageGenerator : MonoBehaviour
+
+
+
+    public GameObject platform;
+    public GameObject platformChest;
+    public GameObject platformInitial;
+    public Transform generationPoint;
+    [SerializeField]
+    private List<GameObject> SpecialPlatforms;
+
+    private int count;
+    private int countSpecial;
+
+    private float platformWidth;
+
+    private int randomPlatform;
+
+    // Use this for initialization
+    void Start()
     {
-        public GameObject platform;
-        public Transform generationPoint;
-        [SerializeField]
-        private List<GameObject> SpecialPlatforms;
+        platformWidth = platformInitial.GetComponent<BoxCollider>().size.x;
+        count = 0;
 
-        private int count;
+    }
 
-        private float platformWidth;
-
-        // Use this for initialization
-        void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
         {
             platformWidth = platform.GetComponent<BoxCollider>().size.x;
-            count = 0;
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (transform.position.x < generationPoint.position.x)
+            transform.position = new Vector3(transform.position.x + platformWidth, transform.position.y, transform.position.z);
+            if (count < 5)
             {
-                transform.position = new Vector3(transform.position.x + platformWidth, transform.position.y, transform.position.z);
-                if (count < 15)
+                Instantiate(platform, transform.position, transform.rotation);
+                count++;
+            }
+            else
+            {
+                countSpecial++;
+                count = 0;
+                if (countSpecial == 5 && SpecialPlatforms.Count != 0)
+                {
+                    countSpecial = 0;
+                    Instantiate(platformChest, transform.position, transform.rotation);
+                }
+                else if (SpecialPlatforms.Count == 0)
                 {
                     Instantiate(platform, transform.position, transform.rotation);
-                    count++;
                 }
                 else
                 {
-                    count = 0;
-                    if (SpecialPlatforms.Count != 0)
-                    {
-                        int randomTeleportPoint = UnityEngine.Random.Range(0, SpecialPlatforms.Count);
-                        Instantiate(SpecialPlatforms[randomTeleportPoint], transform.position, transform.rotation);
-                    }
-                    else
-                    {
-                        Instantiate(platform, transform.position, transform.rotation);
-                    }
-                }
+                    randomPlatform = UnityEngine.Random.Range(0, SpecialPlatforms.Count);
+                    platformWidth = SpecialPlatforms[randomPlatform].GetComponent<BoxCollider>().size.x;
+                    Instantiate(SpecialPlatforms[randomPlatform], transform.position, transform.rotation);
 
+                }
             }
         }
-    } 
+
+    }
 }
+
