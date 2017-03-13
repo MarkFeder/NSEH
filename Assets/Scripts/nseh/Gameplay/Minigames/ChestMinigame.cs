e7c75dee5941d67ffc7bfe312e212879bd574c0f
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-03-13 02:43:29 +0100

    Added some funcionality additional to minigame test scene

---
+++ Assets/Scripts/nseh/Gameplay/Minigames/ChestMinigame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nseh.Gameplay.Movement;
+
+public class ChestMinigame : MonoBehaviour {
+
+    private Collider character;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float randomBuff = Random.value;
+        Debug.Log(randomBuff);
+        character = other;
+        if (randomBuff < 0.25f)
+        {
+            character.GetComponent<Minigame>().velocityCube = -2;
+            //StartCoroutine(ChangeVelocity(other, 1, -2));
+            Invoke("ChangeVelocity", 1f);
+        }
+        else if (randomBuff > 0.25f && randomBuff < 0.5f)
+        {
+            character.GetComponent<Minigame>().velocityCube = -3;
+            //StartCoroutine(ChangeVelocity(other, 1, -2));
+            Invoke("ChangeVelocity", 1f);
+
+        }
+        else if (randomBuff > 0.5f && randomBuff < 0.75f)
+        {
+            character.GetComponent<Minigame>().velocityCube = 2;
+            //StartCoroutine(ChangeVelocity(other, 1, -2));
+            Invoke("ChangeVelocity", 0.1f);
+        }
+        else if (randomBuff > 0.75f && randomBuff < 1f)
+        {
+            character.GetComponent<Minigame>().velocityCube = 3;
+            //StartCoroutine(ChangeVelocity(other, 1, -2));
+            Invoke("ChangeVelocity", 1f);
+        }
+        gameObject.SetActive(false);
+        //Destroy(gameObject);
+    }
+
+    void ChangeVelocity()
+    {
+        character.GetComponent<Minigame>().velocityCube = 0;
+    }
+}
---
+++ Assets/Scripts/nseh/Gameplay/Movement/Minigame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Inputs = nseh.Utils.Constants.Input;
+using nseh.Gameplay.Movement;
+
+namespace nseh.Gameplay.Movement
+{
+
+    public class Minigame : MonoBehaviour
+    {
+
+        public float speed;
+        public float jumpForce;
+        public int velocityCube;
+        private Rigidbody myRigidBody;
+
+        public int gamepadIndex;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRigidBody = GetComponent<Rigidbody>();
+            gamepadIndex = 1;
+            velocityCube = 0;
+            //gamepadIndex = GetComponent<PlayerInfo>().GamepadIndex;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+                myRigidBody.velocity = new Vector3(speed + velocityCube, myRigidBody.velocity.y, myRigidBody.velocity.z);
+            
+            
+           
+            if (Input.GetButtonDown(String.Format("{0}{1}", Inputs.JUMP, this.gamepadIndex)))
+            {
+                myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, jumpForce, myRigidBody.velocity.z);
+            }
+
+        }
+    }
+}
