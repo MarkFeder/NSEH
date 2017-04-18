0184cdb3aaad9f20453d86a4cadace057f0ae2ca
javivecino <javier.vecino.martinez@alumnos.upm.es>
2017-04-18 16:11:59 +0200

    *Fixed a bug on restart from Minigame *Added Player Spawn Point script for a better player spawn/respawn (under construction, not functional yet)

---
+++ Assets/Scripts/nseh/Gameplay/Entities/Environment/PlayerSpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nseh.Managers.Main;
+using nseh.Managers.Level;
+
+namespace nseh.Gameplay.Entities.Environment
+{
+    public class PlayerSpawnPoint : MonoBehaviour
+    {
+        void Start()
+        {
+            GameManager.Instance.Find<LevelManager>().RegisterPlayerSpawnPoint(this.gameObject);
+        }
+    }
+}
