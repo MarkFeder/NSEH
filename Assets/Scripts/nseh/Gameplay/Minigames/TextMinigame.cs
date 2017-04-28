2eac687786387d7bc35ed3091a31e325c4cf4b9b
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-04-28 17:15:21 +0200

    *Changes in the colors of the canvas and player texts

--- Assets/Scripts/nseh/Gameplay/Minigames/TextMinigame.cs
+++ Assets/Scripts/nseh/Gameplay/Minigames/TextMinigame.cs
@@ -13,29 +13,29 @@ using nseh.Gameplay.Movement;
             if (playerText == 1)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
-                this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P1";
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(193, 23, 23, 255);
+            this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P1";
                 this.gameObject.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector3(-0.9f, 2.2f, -0.8f);
             }
 
             else if (playerText == 2)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(0, 70, 255, 255);
-                this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P2";
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(7, 116, 222, 255);
+            this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P2";
             }
 
             else if (playerText == 3)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(128, 0, 255, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(129, 63, 153, 255);
             this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P3";
             }
 
             else if (playerText == 4)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 120, 0, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(229, 124, 22, 255);
             this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P4";
 
             }
--- Assets/Scripts/nseh/Gameplay/Misc/PlayerText.cs
+++ Assets/Scripts/nseh/Gameplay/Misc/PlayerText.cs
@@ -17,7 +17,7 @@ namespace nseh.Gameplay.Misc
             if (playerText == 1)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(193, 23, 23, 255);
                 this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P1";
                 this.gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = "P1";
             }
@@ -25,7 +25,7 @@ namespace nseh.Gameplay.Misc
             else if (playerText == 2)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32 (0, 70, 255, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32 (7, 116, 222, 255);
                 this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P2";
                 this.gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = "P2";
 
@@ -34,7 +34,7 @@ namespace nseh.Gameplay.Misc
             else if (playerText == 3)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32 (128, 0, 255, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32 (129, 63, 153, 255);
                 this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P3";
                 this.gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = "P3";
 
@@ -43,7 +43,7 @@ namespace nseh.Gameplay.Misc
             else if (playerText == 4)
             {
 
-                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 120, 0, 255);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color32(229, 124, 22, 255);
                 this.gameObject.transform.GetChild(0).GetComponent<TextMesh>().text = "P4";
                 this.gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = "P4";
             }
