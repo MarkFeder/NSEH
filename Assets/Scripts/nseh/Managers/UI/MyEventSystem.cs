7d5b4e7def57dfe6db6a6f5e31080963a3e0a73d
DESKTOP-RM7K3NL\Talf <j.alvarez-campana@alumnos.upm.es>
2017-07-13 18:22:31 +0200

    Primeros pasos en eventsystem multiple

---
+++ Assets/Scripts/nseh/Managers/UI/MyEventSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.EventSystems;
+
+public class MyEventSystem : EventSystem
+{
+    protected override void OnEnable()
+    {
+        // do not assign EventSystem.current
+    }
+
+    protected override void Update()
+    {
+        EventSystem originalCurrent = current;
+        current = this; // in order to avoid reimplementing half of the EventSystem class, just temporarily assign this EventSystem to be the globally current one
+        base.Update();
+        current = originalCurrent;
+    }
+}
